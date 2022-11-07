b2387d175a3fa812a7043c495519897f2ea95b4b
nhaajtddas <nhaajtddas@gmail.com>
2022-11-07 17:11:57 +0700

    update admin

--- src/main/java/com/fptpoly/main/Controller/WebMainController.java
+++ src/main/java/com/fptpoly/main/Controller/WebMainController.java
@@ -297,5 +297,74 @@ public class WebMainController {
      public String quenmatkhau(Model model){
           return "site/security/quenmatkhau";
      }
-     
+      // admin
+
+      @RequestMapping("/admin")
+      public String admin(Model model){
+           return "admin/layouts/index"; 
+      }
+      // Product
+      @RequestMapping("admin/product-car")
+      public String product_car(Model model){
+           return "admin/pages/E-commerce/products/product-car"; 
+      }
+      @RequestMapping("admin/product-phukien")
+      public String product_phukien(Model model){
+           return "admin/pages/E-commerce/products/product-phukien"; 
+      }
+      @RequestMapping("admin/car-detail")
+      public String car_detail(Model model){
+           return "admin/pages/E-commerce/products/car-details"; 
+      }
+      @RequestMapping("admin/access-detail")
+      public String access_detail(Model model){
+           return "admin/pages/E-commerce/products/access-details"; 
+      }
+      @RequestMapping("admin/add-car")
+      public String car_add(Model model){
+           return "admin/pages/E-commerce/products/add-car"; 
+      }
+      @RequestMapping("admin/add-access")
+      public String access_add(Model model){
+           return "admin/pages/E-commerce/products/add-access"; 
+      }
+      // Oders
+      @RequestMapping("admin/orders-car")
+      public String orders_car(Model model){
+           return "admin/pages/E-commerce/orders/orders-car"; 
+      }
+      @RequestMapping("admin/orders-accessories")
+      public String orders_access(Model model){
+           return "admin/pages/E-commerce/orders/orders-accessories"; 
+      }
+      @RequestMapping("admin/orders-accessories-detail")
+      public String orders_detail(Model model){
+           return "admin/pages/E-commerce/orders/orders-accessories-details"; 
+      }
+      // users
+      @RequestMapping("admin/users-add") 
+      public String users_add(Model model){
+           return "admin/pages/users/users-add-user"; 
+      }
+      @RequestMapping("admin/users") 
+      public String users(Model model){
+           return "admin/pages/users/users"; 
+      }
+      @RequestMapping("admin/user-profile") 
+      public String user_profile(Model model){
+           return "admin/pages/users/user-profile"; 
+      }
+      @RequestMapping("admin/user-my-profile") 
+      public String user_my_profile(Model model){
+           return "admin/pages/users-profile/user-my-profile"; 
+      }
+      @RequestMapping("admin/user-edit") 
+      public String user_edit(Model model){
+           return "admin/pages/accounts/account-settings"; 
+      }
+      // welcome
+      @RequestMapping("admin/welcome")
+      public String welcome(Model model){
+           return "admin/welcome-page"; 
+      }
 }
